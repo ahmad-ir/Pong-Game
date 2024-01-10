9f217f2e2b1f427ebf3c1f33ad085ec8c6955355
Ahmad <amiriahmadir@gmail.com>
2024-01-10 16:10:58 +0100

    Added ball interaction with borders and paddles and score line

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+# Scores
+score_a = 0
+score_b = 0
+
 # Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
@@ -32,7 +36,22 @@ ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
+ball.dx = 0.1 
+ball.dy = 0.1
 
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write(
+    f"Player A: {score_a} Player B: {score_b}", 
+    align = "center",
+    font = ('Courier', 24, "normal"))
+
+# Functions
 def paddle_a_up():
     y = paddle_a.ycor()
     y += 20
@@ -65,4 +84,51 @@ wn.onkeypress(paddle_b_down, "Down")
 #Main game loop
 while True:
     wn.update()
+
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
     
+    # Border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+
+    if ball.xcor() > 390:
+        ball.goto(0,0)
+        ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write(
+            f"Player A: {score_a} Player B: {score_b}", 
+            align = "center",
+            font = ('Courier', 24, "normal"))
+
+    if ball.xcor() < -390:
+        ball.goto(0,0)
+        ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write(
+            f"Player A: {score_a} Player B: {score_b}", 
+            align = "center",
+            font = ('Courier', 24, "normal"))
+
+    # Ball and paddle b collision
+    if (ball.xcor() > 340 and ball.xcor() < 350) \
+        and (ball.ycor() < paddle_b.ycor()+40 \
+        and ball.ycor() > paddle_b.ycor()-40):
+        ball.setx(340)
+        ball.dx *= -1
+
+    # Ball and paddle a collision
+    if (ball.xcor() < -340 and ball.xcor() > -350) \
+        and (ball.ycor() < paddle_a.ycor()+40 \
+        and ball.ycor() > paddle_a.ycor()-40):
+        ball.setx(-340)
+        ball.dx *= -1
+
